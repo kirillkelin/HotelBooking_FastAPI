8ae3b1de71ab6a8f3acefee0229c0d0ef8eba929
kirillkelin <kelinkirill@gmail.com>
2023-10-08 23:35:49 +0300

    Создание токена и верификации пароля

--- app/users/auth.py
+++ app/users/auth.py
@@ -1,4 +1,11 @@
+from datetime import datetime, timedelta
+from jose import jwt
 from passlib.context import CryptContext
+from pydantic import EmailStr
+from app.config import settings
+
+from app.users.dao import UsersDAO
+
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -7,3 +14,19 @@ def get_password_hash(password: str) -> str:
 
 def verify_password(plain_password, hashed_password) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=30)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(
+        to_encode, settings.SECRET_KEY, settings.SECRET_ALGORITHM 
+    )
+    return encoded_jwt
+
+async def authenticate_user(email: EmailStr, password: str):
+    user = await UsersDAO.find_one_or_none(email=email)
+    if not(user and verify_password(password, user.hashed_password)):
+        return None
+    return user
+
