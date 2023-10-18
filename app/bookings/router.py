9706ef07679e958b8ba0664cc4af029e3190080d
kirillkelin <kelinkirill@gmail.com>
2023-10-18 21:03:04 +0300

    Создание фоновых задач

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,8 +1,10 @@
 from datetime import date
 from fastapi import APIRouter, Depends
+from pydantic import TypeAdapter
 from app.bookings.dao import BookingDAO
-from app.bookings.schemas import SBookingInfo
+from app.bookings.schemas import SBooking, SBookingInfo
 from app.exceptions import RoomCannotBeBooked
+from app.tasks.tasks import send_booking_confirmation_email
 from app.users.dependencies import get_current_user
 from app.users.models import Users
 
@@ -25,6 +27,10 @@ async def add_booking(
     booking = await BookingDAO.add(user.id, room_id, date_from, date_to)
     if not booking:
         raise RoomCannotBeBooked
+    booking_dict = TypeAdapter(SBooking).validate_python(booking).model_dump()
+    send_booking_confirmation_email.delay(booking_dict, user.email)
+    return booking_dict
+
     
 
 @router.delete("/{booking_id}")
