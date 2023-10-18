63dafc50e70d51c4368cfae5046de1ec729b13c0
kirillkelin <kelinkirill@gmail.com>
2023-10-18 21:03:37 +0300

    Подключение фронтенда и redis

--- app/main.py
+++ app/main.py
@@ -1,52 +1,45 @@
-from fastapi import FastAPI, Query, Depends
-from typing import Optional
-from datetime import date
-from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from app.bookings.router import router as router_bookings
 from app.users.router import router as router_users
 from app.hotels.router import router as router_hotels
 from app.hotels.rooms.router import router as router_hotels_rooms
+from app.pages.router import router as router_pages
+from app.images.router import router as router_images
+from app.config import settings
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from fastapi_cache.decorator import cache
+from redis import asyncio as aioredis
 
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="app/static"), "static")
 
 app.include_router(router_users)
 app.include_router(router_bookings)
 app.include_router(router_hotels)
 app.include_router(router_hotels_rooms)
+app.include_router(router_pages)
+app.include_router(router_images)
 
-class HotelsSearchArgs:
-    def __init__(
-        self,
-        location: str,
-        date_from: date, 
-        date_to: date,
-        has_spa: Optional[bool] = None,
-        stars: Optional[int] = Query(None, ge = 1, le = 5),
-    ):
-        self.location = location
-        self.date_from = date_from
-        self.date_to = date_to
-        self.has_spa = has_spa
-        self.stars = stars
+origins = [
+    "http://localhost:3000",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["*"],
+)
 
 
-class SHotel(BaseModel):
-    address: str
-    name: str
-    stars: int
+@app.on_event("startup")
+def startup():
+    redis = aioredis.from_url(f"redis://{settings.REDIS_HOST}", encoding="utf8", decode_responses=True)
+    FastAPICache.init(RedisBackend(redis), prefix="cache")
 
-
-@app.get("/hotels")
-def get_hotels(
-    search_args: HotelsSearchArgs = Depends()
-) -> list[SHotel]:
-    hotels = [
-        {
-            "address": "ул. Пушкина, 3, Казань",
-            "name": "Super Hotel",
-            "stars": 5,
-        }
-    ]
-    return hotels
