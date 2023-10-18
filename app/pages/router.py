fd6aba592780c58f27cdfc008e6c1211d921f008
kirillkelin <kelinkirill@gmail.com>
2023-10-18 21:00:25 +0300

    Работа с фронтендом

---
+++ app/pages/router.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, Depends, Request
+from fastapi.templating import Jinja2Templates
+
+from app.hotels.router import get_hotel
+
+router = APIRouter(
+    prefix="/pages",
+    tags = ["Фронтенд"]
+)
+
+templates = Jinja2Templates(directory="app/templates")
+
+@router.get("/hotels")
+async def get_hotels_page(
+    request: Request,
+    hotels=Depends(get_hotel)
+):
+    return templates.TemplateResponse(
+        name="hotels.html", 
+        context={"request": request, "hotels": hotels}
+    )
