3ebca2e02414f3069cf8e3d959add4fb3ec399f1
kirillkelin <kelinkirill@gmail.com>
2023-10-19 23:55:33 +0300

    Создание админки

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from app.admin.views import BookingsAdmin, HotelsAdmin, RoomsAdmin, UsersAdmin
 from app.bookings.router import router as router_bookings
+from app.users.models import Users
 from app.users.router import router as router_users
 from app.hotels.router import router as router_hotels
 from app.hotels.rooms.router import router as router_hotels_rooms
@@ -12,7 +14,9 @@ from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 from fastapi_cache.decorator import cache
 from redis import asyncio as aioredis
-
+from sqladmin import Admin, ModelView
+from app.database import engine
+from app.admin.auth import authentication_backend
 
 app = FastAPI()
 
@@ -43,3 +47,11 @@ def startup():
     redis = aioredis.from_url(f"redis://{settings.REDIS_HOST}", encoding="utf8", decode_responses=True)
     FastAPICache.init(RedisBackend(redis), prefix="cache")
 
+
+admin = Admin(app, engine, authentication_backend=authentication_backend)
+
+
+admin.add_view(UsersAdmin)
+admin.add_view(BookingsAdmin)
+admin.add_view(RoomsAdmin)
+admin.add_view(HotelsAdmin)
