307e05543c1961248a7a27ec197fbf8732367a26
kirillkelin <kelinkirill@gmail.com>
2023-10-24 15:47:11 +0300

    Настройка среды для написания тестов

--- app/config.py
+++ app/config.py
@@ -1,12 +1,21 @@
+from typing import Literal
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    MODE: Literal["DEV", "TEST", "PROD"]
+
     DB_HOST: str
     DB_PORT: int
     DB_USER: str
     DB_PASS: str
     DB_NAME: str 
+
+    TEST_DB_HOST: str
+    TEST_DB_PORT: int
+    TEST_DB_USER: str
+    TEST_DB_PASS: str
+    TEST_DB_NAME: str 
 
     SECRET_KEY: str
     SECRET_ALGORITHM: str
@@ -21,9 +30,15 @@ class Settings(BaseSettings):
     ADMIN_MAIL: str
 
     @property
-    def database_url(self):
+    def get_database_url(self):
         user = f"{self.DB_USER}:{self.DB_PASS}"
         database = f"{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        return f"postgresql+asyncpg://{user}@{database}"
+    
+    @property
+    def get_test_database_url(self):
+        user = f"{self.TEST_DB_USER}:{self.TEST_DB_PASS}"
+        database = f"{self.TEST_DB_HOST}:{self.TEST_DB_PORT}/{self.TEST_DB_NAME}"
         return f"postgresql+asyncpg://{user}@{database}"
     
 
--- app/database.py
+++ app/database.py
@@ -1,9 +1,18 @@
+from sqlalchemy import NullPool
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 from app.config import settings
 
-engine = create_async_engine(settings.database_url) #создаем асинхронный движок для передачи URL
+
+if settings.MODE == "TEST":
+    DATABASE_URL = settings.get_test_database_url
+    DATABASE_PARAMS = {"poolclass": NullPool}
+else:
+    DATABASE_URL = settings.get_database_url
+    DATABASE_PARAMS = {}
+
+engine = create_async_engine(DATABASE_URL, **DATABASE_PARAMS) #создаем асинхронный движок для передачи URL
 
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False) #создение сессии
 
