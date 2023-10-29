fcb7f00e56c8205f6af68f9efb47f3c02da1da67
kirillkelin <kelinkirill@gmail.com>
2023-10-30 01:13:02 +0300

    Добавление стилизации кода

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -11,10 +11,10 @@ sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
 
 from app.config import settings
 from app.database import Base
-from app.hotels.models import Hotels
-from app.bookings.models import Bookings
-from app.users.models import Users
-from app.hotels.rooms.models import Rooms
+from app.hotels.models import Hotels # noqa
+from app.bookings.models import Bookings # noqa
+from app.users.models import Users # noqa
+from app.hotels.rooms.models import Rooms # noqa
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
